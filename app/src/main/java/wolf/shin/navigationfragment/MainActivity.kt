56b795331542f7bbfa1c10cf69c1e72c61b30655
shindonghwi <ehdgnl8940@naver.com>
2022-05-22 11:47:01 +0900

    feat: bottomNavigation - NavController Setting

--- app/src/main/java/wolf/shin/navigationfragment/MainActivity.kt
+++ app/src/main/java/wolf/shin/navigationfragment/MainActivity.kt
@@ -2,10 +2,21 @@ package wolf.shin.navigationfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import wolf.shin.navigationfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(_binding.mainBottomNavigation, navController)
     }
 }
