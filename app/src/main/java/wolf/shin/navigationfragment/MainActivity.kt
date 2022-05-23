4ef3ccbea47f6d9e835d3cb77abbf51f1a176589
shindonghwi <ehdgnl8940@naver.com>
2022-05-23 10:54:16 +0900

    fix: bottomNavigation 바텀메뉴 탐색기능 수정

--- app/src/main/java/wolf/shin/navigationfragment/MainActivity.kt
+++ app/src/main/java/wolf/shin/navigationfragment/MainActivity.kt
@@ -2,15 +2,14 @@ package wolf.shin.navigationfragment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.*
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import wolf.shin.navigationfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var _binding: ActivityMainBinding
-    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,27 +17,9 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_container
-        ) as NavHostFragment
-        navController = navHostFragment.navController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        _binding.bottomNavigation.setupWithNavController(navController)
 
-        appBarConfiguration = AppBarConfiguration(
-            navGraph = navController.graph
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        _binding.bottomNavigation.apply {
-            navController.let { navController ->
-                NavigationUI.setupWithNavController(this, navController)
-                setOnItemSelectedListener { item ->
-                    NavigationUI.onNavDestinationSelected(item, navController)
-                    true
-                }
-                setOnItemReselectedListener {
-                    navController.popBackStack(destinationId = it.itemId, inclusive = false)
-                }
-            }
-        }
     }
 }
